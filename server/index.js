67bb4709efdd737b65958506a5c6d5f95cef3d9a
Nahid-07 <nahidasif1998@gmail.com>
2023-01-04 23:55:19 +0600

    jwt token added and user email sent to the DB

--- server/index.js
+++ server/index.js
@@ -1,38 +1,62 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+jwt = require("jsonwebtoken");
 
-const app = express()
-const port = process.env.PORT || 8000
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Database Connection
-const uri = process.env.DB_URI
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ugpmzsn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-})
+});
 
 async function run() {
   try {
-    const homesCollection = client.db('aircncdb').collection('homes')
+    const homesCollection = client.db("aircncdb").collection("homes");
+    const usersCollection = client.db("aircncdb").collection("users");
 
-    console.log('Database Connected...')
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+
+      const token = jwt.sign(user, process.env.TOKEN, { expiresIn: "1d" });
+      console.log(token);
+      res.send({ result, token });
+    });
+
+    console.log("Database Connected...");
   } finally {
   }
 }
 
-run().catch(err => console.error(err))
+run().catch((err) => console.error(err));
 
-app.get('/', (req, res) => {
-  res.send('Server is running...')
-})
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running...on ${port}`)
-})
+  console.log(`Server is running...on ${port}`);
+});
