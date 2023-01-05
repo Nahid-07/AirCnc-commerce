6aa8d270cf28737270d8879cf392828bd4e84569
Nahid-07 <nahidasif1998@gmail.com>
2023-01-05 12:17:23 +0600

    search field added

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import Spinner from '../Components/Spinner/Spinner'
+import React from "react";
+import Spinner from "../Components/Spinner/Spinner";
+import SearchForm from '../Components/Form/SearchForm'
 const Home = () => {
   return (
-    <>
-      <Spinner />
-    </>
-  )
-}
+    <div className="md:flex justify-center gap-10 px-6 md:px-10 lg:px-20">
+      <div className="mt-4">
+        <SearchForm/>
+      </div>
+      <div className="flex-1">
+        <div>Home items</div>
+        <div>Experience items</div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
