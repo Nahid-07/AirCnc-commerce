402eb393657611844c5f2c2263ceb3490f894b52
Nahid-07 <nahidasif1998@gmail.com>
2023-01-21 11:36:10 +0600

    my bookings added

--- client/src/api/booking.js
+++ client/src/api/booking.js
@@ -9,3 +9,17 @@ export const saveBookingData = async (bookingData) => {
   const data = await response.json();
   return data;
 };
+
+// getting all the bookings by user email
+export const getBookingsByEmail = async (email) => {
+  const response = await fetch(`http://localhost:5000/bookings?email=${email}`);
+  const data = await response.json();
+  return data;
+};
+
+// getting all the bookings by user email
+export const getBookings = async () => {
+  const response = await fetch(`http://localhost:5000/bookings`);
+  const data = await response.json();
+  return data;
+};
