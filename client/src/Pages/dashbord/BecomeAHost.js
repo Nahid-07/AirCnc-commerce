76c41f87e7a3e4c10fd896c8b29ebe66ef602309
Nahid-07 <nahidasif1998@gmail.com>
2023-01-23 23:38:46 +0600

    become host form added

--- client/src/Pages/dashbord/BecomeAHost.js
+++ client/src/Pages/dashbord/BecomeAHost.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import BecomeAHostForm from '../../Components/Form/BecomeAHostForm';
 
 const BecomeAHost = () => {
     return (
-        <div>
-            
-        </div>
+        <BecomeAHostForm />
     );
 };
 
