c138f88bb8ff985d525872a260e3f3d94091c6c8
Nahid-07 <nahidasif1998@gmail.com>
2023-01-24 23:40:45 +0600

    become host function added

--- client/src/Pages/dashbord/BecomeAHost.js
+++ client/src/Pages/dashbord/BecomeAHost.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import BecomeAHostForm from '../../Components/Form/BecomeAHostForm';
+import React from "react";
+import { getImageUrl } from "../../api/imageUpload";
+import BecomeAHostForm from "../../Components/Form/BecomeAHostForm";
 
 const BecomeAHost = () => {
-    const handleSubmit = event => {
-        event.preventDefault();
-        const location = event.target.location.value;
-        const image = event.target.image.file[0];
-        const hostData = {
-            location: location,
-            image: image
-        }
-    }
-    return (
-        <BecomeAHostForm handleSubmit={handleSubmit} />
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const location = event.target.location.value;
+    const image = event.target.image.files[0];
+
+    // image host url
+    getImageUrl(image).then((data) => {
+      const hostData = {
+        location: location,
+        image: data,
+      };
+      console.log(hostData);
+    });
+  };
+  return <BecomeAHostForm handleSubmit={handleSubmit} />;
 };
 
 export default BecomeAHost;
