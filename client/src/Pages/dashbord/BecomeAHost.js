e1d582720db40befadafd1e98222a8c98a9b3e78
Nahid-07 <nahidasif1998@gmail.com>
2023-01-26 23:46:53 +0600

    host requested object created

--- client/src/Pages/dashbord/BecomeAHost.js
+++ client/src/Pages/dashbord/BecomeAHost.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useContext } from "react";
 import { getImageUrl } from "../../api/imageUpload";
 import BecomeAHostForm from "../../Components/Form/BecomeAHostForm";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const BecomeAHost = () => {
+  const {user} = useContext(AuthContext)
   const handleSubmit = (event) => {
     event.preventDefault();
     const location = event.target.location.value;
@@ -12,7 +15,9 @@ const BecomeAHost = () => {
     getImageUrl(image).then((data) => {
       const hostData = {
         location: location,
-        image: data,
+        documentImg: data,
+        role: "requested",
+        email: user?.email
       };
       console.log(hostData);
     });
